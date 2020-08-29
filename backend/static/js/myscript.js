af11b90d3697ba35aafadf51d97fbb2d0d96f76e
DangZRQ <3170105144@zju.edu.cn>
2020-08-29 22:42:37 +0800

    frontend_debug_ok

--- backend/static/js/myscript.js
+++ backend/static/js/myscript.js
@@ -1,13 +1,65 @@
-import {
-    Refresh,
-    RefreshN,
-} from './utils/requests.js';
+async function GetLatestCommit() {
+    let response = await fetch('/latestcommit');
+    let result = await response.text();
+    console.log('GetLatestCommit: ' + result);
+    var l = 0;
+    var r = result.length;
+    while (result[l] == "\"") l = l+1;
+    while (result[r-1] == "\n" || result[r-1]=="\"") r = r-1;
+    result = result.substr(l, r-l);
+    return result;
+}
+
+async function GetCommitInfo(commit_id) {
+    let data = {
+        commit_id: commit_id
+    };
+    let response = await fetch('/commitinfo', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    let result = await response.json();
+    console.log('GetCommitInfo: ',result);
+    return result;
+}
+
+async function GetNCommitsInfo(commit_id, num) {
+    let data = {
+        latest_commit_id: commit_id,
+        n_commit_info: num
+    };
+    let response = await fetch('/ncommitinfo', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    let result = await response.json();
+    console.log('GetNCommitsInfo: ',result);
+    return result;
+}
+
+async function Refresh(commit_id = "") {
+    if (commit_id == "") commit_id = await GetLatestCommit();
+    let data = await GetCommitInfo(commit_id);
+    return data;
+}
+
+async function RefreshN(commit_id = "", num = 10) {
+    if (commit_id == "") commit_id = await GetLatestCommit();
+    let data = await GetNCommitsInfo(commit_id, num);
+    return data;
+}
 
 // Return 一个list，长度为2，两个元素都是list
 // 第一个list元素是commit-id列表
 // 第二个list是每一次commit的详细数据
 //      每一次commit详细数据构成：一个map，文件名和对应的数据使用情况
-export async function GetDataList(commit_id = "", num = 10) {
+async function GetDataList(commit_id = "", num = 10) {
     let data = await RefreshN(commit_id, num);
     var result = [];
     var len = data.length;
@@ -41,7 +93,7 @@ export async function GetDataList(commit_id = "", num = 10) {
 
 // 返回一个Map，5个key，分别是commit_id、text、bss、data、total
 // 每个key对应一个list，每个元素是一个值。
-export async function GetDataListBySummary(commit_id = "", num = 10) {
+async function GetDataListBySummary(commit_id = "", num = 10) {
     let data = await RefreshN(commit_id, num);
     var result = {};
     var len = data.length;
@@ -64,7 +116,7 @@ export async function GetDataListBySummary(commit_id = "", num = 10) {
         }
         text_.push(text_summary);
         bss_.push(bss_summary);
-        data_.push(total_summary);
+        data_.push(data_summary);
         total_.push(total_summary);
     }
     result["commit_id"] = commit_id_;
@@ -76,7 +128,7 @@ export async function GetDataListBySummary(commit_id = "", num = 10) {
 }
 
 // 用list套list的方式返回值，顺序需要注意，可以自行或叫zjw修改
-export async function GetDataListBySummaryList(commit_id = "", num = 10) {
+async function GetDataListBySummaryList(commit_id = "", num = 10) {
     let data = await GetDataListBySummary(commit_id, num);
     var result = [];
     result.push(data["commit_id"]);
